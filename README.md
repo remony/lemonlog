2c93de2a913e1fb57e60a1fc198e44fb0cf718ce
remon <remonasebi@gmail.com>
2016-01-11 01:27:51 +0000

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lemon Log
+
+A simple Goji v1 middleware to provide logging
+
+> Please note this is provided and personalized towards my Honours project however open to modifications.
+
+- Displays **METHOD**, **Time between request and response** and **Reques URL**
+- Current displays only when path == ```/api```
+
+## Possible extensions
+- Provide filters instead of just /api
---
+++ main.go
@@ -0,0 +1,26 @@
+package lemonlog
+
+import (
+	"net/http"
+	"github.com/zenazn/goji/web"
+	"time"
+	"fmt"
+)
+
+func Logger(c *web.C, h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, req *http.Request) {
+		t1 := time.Now()
+
+		h.ServeHTTP(w, req)
+		t2 := time.Now()
+		//w.Write("<script>alert('hello')</script>")
+		path:=req.URL.String()
+		if (len(path) >3) {
+			if (path[:4] == "/api") {
+				fmt.Printf("[%-3s]%-10s|%-6s\n", req.Method, t2.Sub(t1), req.URL.String())
+			}
+		}
+
+	}
+	return http.HandlerFunc(fn)
+}
